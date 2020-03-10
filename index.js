81ea04b380e259b138cb155a88b2e1a352524d6d
Nick Tang <tangnick@live.com>
2020-03-09 22:02:21 -0400

    clapify function

---
+++ index.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+const [,, ...args] = process.argv;
+
+const clapify = (str) => {
+    return str.reduce((acc, current) => {
+        return acc + ' 👏 ' + current;
+    });
+};
+
+const result = clapify(args);
+console.log(result);
